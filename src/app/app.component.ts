ad296d57ddb2cd85fe7e2e024b25cd0453dd1da8
Grzegorz <grzegorz.sokolowski1997@gmail.com>
2020-05-21 19:24:58 +0200

    Checking if employee's role equals task's type

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import {HttpClient, HttpParams} from '@angular/common/http';
+import {Employee} from './employee';
+import {Task} from './task';
+import {Meeting} from './meeting';
+import {Observable} from 'rxjs';
+import {concatMap, tap} from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +14,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'TaskManagerAngular';
+
+  constructor(private httpClient: HttpClient) {}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import {RouterModule, Routes} from '@angular/router';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { TaskListComponent } from './task-list/task-list.component';
+import { MeetingListComponent } from './meeting-list/meeting-list.component';
+import {Meeting} from './meeting';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'customer', pathMatch: 'full' },
+  { path: 'employee', component: EmployeeListComponent },
+  { path: 'meeting', component: MeetingListComponent },
+  { path: 'task', component: TaskListComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeListComponent,
+    TaskListComponent,
+    MeetingListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes)
   ],
+  exports: [RouterModule],
   providers: [],
   bootstrap: [AppComponent]
 })
